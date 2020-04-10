36ddab05db5d8c04a3b27c18a2f411b72ddbfd17
jithinolickal <jithinolickal@gmail.com>
2020-04-10 13:10:16 +0530

    added material ui Tabs

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div className="footerDiv">
+hello
+            </div>
+        );
+    }
+}
+export default Footer;
---
+++ src/components/HomeBody.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class HomeBody extends React.Component{
+    render(){
+        return(
+            <div className="bodyDiv">
+body
+            </div>
+        );
+    }
+}
+export default HomeBody;
